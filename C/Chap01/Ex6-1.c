46c1db90aadd49644a35fc90365bbe728bc6e22d
Virtual Sense <virtual_sense@naver.com>
2025-11-03 11:55:14 +0900

    Create Ex6-1.c

---
+++ C/Chap01/Ex6-1.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	// 1 ~ 10까지 출력하는 while을 작성하세요!!!
+	int i = 0;
+
+	while (i < 10) {
+		i++;
+		printf("i : %d\n", i);
+		
+	}
+
+
+	return 0;
+}
